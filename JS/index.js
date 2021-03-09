922ca0636c5c94465bbb51ce72249b8203e394ab
fidellim <lim.fidel@gmail.com>
2021-03-09 19:17:01 +0400

    add responsiveness

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+// var navElem = document.getElementById("nav-header");
+// var homeHeaderElem = document.getElementById("header");
+
+// var homeContentHeight = homeHeaderElem.offsetHeight - navElem.offsetHeight;
+// console.log(homeContentHeight);
+
+// document.getElementById(
+// 	"header-content"
+// ).style.height = `${homeContentHeight}px`;
